872c5f50ebe059902148c3456fc1d58c57b18280
Matthew Dodi <matthew.dodi@gmail.com>
2018-07-21 16:50:40 -0700

    created new variables and printed out content with 'echo'

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+    <?php
+    $characterName = "John";
+    $characterAge = 35;
+
+    echo("
+        <p>There once was a man named $characterName <br>
+        He was $characterAge years old <br>
+        He really liked the name $characterName <br>
+        But didn't like being $characterAge</p>
+        ");
+    ?>
+
+
+</body>
+</html>
